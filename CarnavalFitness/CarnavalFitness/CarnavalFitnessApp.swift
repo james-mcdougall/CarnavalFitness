0f31f7066416f5e3957c9883d1f642e2961277fa
james.mcdougall <jameswilliammcdougall@gmail.com>
2023-05-15 19:54:46 -0700

    Initial Project Commit

---
+++ CarnavalFitness/CarnavalFitness/CarnavalFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnavalFitnessApp.swift
+//  CarnavalFitness
+//
+//  Created by James McDougall on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CarnavalFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarnavalFitness/CarnavalFitness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarnavalFitness
+//
+//  Created by James McDougall on 5/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
